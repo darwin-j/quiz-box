aa7990cd8f9d77eeaf8881ee8a3a4d56bf1cddd2
Your Name <darwinmuthu450@gmail.com>
2021-06-26 18:23:44 +0530

    database added (crud for usersData done)

---
+++ firebase/context.js
@@ -0,0 +1,51 @@
+import { createContext } from "react";
+import { firebaseDB } from "./inti";
+
+export const firebaseContext = createContext();
+
+export const FirebaseProvider = (props) => {
+  const createDB = (DBname, docID, docData) => {
+    firebaseDB.collection(DBname).doc(docID).set(docData);
+  };
+
+  const write = (DBname, docID, docData) => {
+    firebaseDB.collection(DBname).doc(docID).set(docData);
+  };
+
+  const readAllData = (DBname) => {
+    const DBref = firebaseDB.collection(DBname);
+    DBref.onSnapshot((querySnapshot) => {
+      const dataArry = [];
+      querySnapshot.forEach((doc) => {
+        dataArry.push(doc.data());
+        console.log(dataArry);
+      });
+    });
+  };
+
+  const readData = (DBname, docID) => {
+    const docRef = firebaseDB.collection(DBname).doc(docID);
+
+    docRef
+      .get()
+      .then((doc) => {
+        console.log(doc.data());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const deleteData = (DBname, docID) => {
+    const docRef = firebaseDB.collection(DBname).doc(docID);
+    docRef.delete();
+  };
+
+  return (
+    <firebaseContext.Provider
+      value={[createDB, write, readAllData, readData, deleteData]}
+    >
+      {props.children}
+    </firebaseContext.Provider>
+  );
+};
